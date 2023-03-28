f9b71d811b4285f6823e49bb7202b3cd560a2334
oiram123 <bishainter78@yahoo.com>
2023-03-28 22:03:26 +0200

    bug fixed

--- models/todo.js
+++ models/todo.js
@@ -1,19 +1,27 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const TodoSchema = new Schema({
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    title: {
-        type: string
-    },
-    content:{
-        type: string,
-    }
+  createdAt: {
+      type: Date,
+      default: Date.now,
+  },
+  title: {
+      type: String
+  },
+  content:{
+      type: String,
+  },
+  status: {
+    default: "Todo",
+    type: String,
+  },
+    user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
 })
 
 
-const Todo = mongoose.model("Todo", TodoSchema)
+const Todo = mongoose.model("Todo", TodoSchema);
 module.exports = Todo;
